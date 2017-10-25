a213aa44b34690b44639e9fac4eb801d91567d72
Kimberly Bordon <kbordon@gmail.com>
2017-10-24 21:39:35 -0700

    Add edit, and delete functionality to tasks.

---
+++ ToDoList.Tests/ModelTests/TaskTest.cs
@@ -0,0 +1,166 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+
+    [TestClass]
+    public class TaskTests : IDisposable
+    {
+        public TaskTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=todo_test;";
+        }
+        public void Dispose()
+        {
+          Task.DeleteAll();
+          Category.DeleteAll();
+        }
+
+        [TestMethod]
+        public void Equals_OverrideTrueForSameDescription_Task()
+        {
+          //Arrange, Act
+          DateTime newDateTime = new DateTime(2016,12,31);
+          Task firstTask = new Task("Mow the lawn", 1, newDateTime, newDateTime.ToString());
+          Task secondTask = new Task("Mow the lawn", 1, newDateTime, newDateTime.ToString());
+
+          //Assert
+          Assert.AreEqual(firstTask, secondTask);
+        }
+
+        [TestMethod]
+        public void Save_SavesTaskToDatabase_TaskList()
+        {
+          //Arrange
+          DateTime newDateTime = new DateTime(2016,12,31);
+          Task testTask = new Task("Mow the lawn", 1, newDateTime, newDateTime.ToString());
+          testTask.Save();
+
+          //Act
+          List<Task> result = Task.GetAll();
+          List<Task> testList = new List<Task>{testTask};
+
+          //Assert
+          CollectionAssert.AreEqual(testList, result);
+        }
+       [TestMethod]
+        public void Save_DatabaseAssignsIdToObject_Id()
+        {
+          //Arrange
+          DateTime newDateTime = new DateTime(2016,12,31);
+          Task testTask = new Task("Mow the lawn", 1, newDateTime, newDateTime.ToString());
+          testTask.Save();
+
+          //Act
+          Task savedTask = Task.GetAll()[0];
+
+          int result = savedTask.GetId();
+          int testId = testTask.GetId();
+
+          //Assert
+          Assert.AreEqual(testId, result);
+        }
+
+        [TestMethod]
+        public void Find_FindsTaskInDatabase_Task()
+        {
+          //Arrange
+          DateTime newDateTime = new DateTime(2016,12,31);
+          Task testTask = new Task("Mow the lawn", 1, newDateTime, newDateTime.ToString());
+          testTask.Save();
+
+          //Act
+          Task foundTask = Task.Find(testTask.GetId());
+
+          //Assert
+          Assert.AreEqual(testTask, foundTask);
+        }
+
+        [TestMethod]
+        public void GetTasks_RetrievesAllTasksWithCategory_TaskList()
+        {
+          Category testCategory = new Category("Household chores");
+          testCategory.Save();
+
+          DateTime newDateTime = new DateTime(2016,12,31);
+          DateTime newSecondDateTime = new DateTime(2017,1,1);
+          Task firstTask = new Task("Mow the lawn", testCategory.GetId(), newDateTime, newDateTime.ToString());
+          firstTask.Save();
+          Task secondTask = new Task("Do the dishes", testCategory.GetId(), newSecondDateTime, newSecondDateTime.ToString());
+          secondTask.Save();
+
+
+          List<Task> testTaskList = new List<Task> {firstTask, secondTask};
+          List<Task> resultTaskList = testCategory.GetTasks();
+
+          CollectionAssert.AreEqual(testTaskList, resultTaskList);
+        }
+
+        [TestMethod]
+        public void GetTasks_MakeSureListOrderedByDate_TaskList()
+        {
+          Category testCategory = new Category("Household chores");
+          testCategory.Save();
+
+          DateTime newDateTime = new DateTime(2016,12,31);
+          DateTime newSecondDateTime = new DateTime(2015,1,1);
+          Task firstTask = new Task("Mow the lawn", testCategory.GetId(), newDateTime, newDateTime.ToString());
+          firstTask.Save();
+          Task secondTask = new Task("Do the dishes", testCategory.GetId(), newSecondDateTime, newSecondDateTime.ToString());
+          secondTask.Save();
+
+          List<Task> testTaskList = new List<Task> {secondTask, firstTask};
+          List<Task> resultTaskList = testCategory.GetTasks();
+
+          CollectionAssert.AreEqual(testTaskList, resultTaskList);
+        }
+
+        [TestMethod]
+        public void UpdateDescription_UpdatesTaskInDatabase_String()
+        {
+          //Arrange
+          string description = "Walk the Dog";
+          DateTime newDateTime = new DateTime(2016,12,31);
+          Task testTask = new Task(description, 1, newDateTime, newDateTime.ToString());
+          testTask.Save();
+          string newDescription = "Mow the lawn";
+
+          //Act
+          testTask.UpdateDescription(newDescription);
+
+          string result = Task.Find(testTask.GetId()).GetDescription();
+          Console.WriteLine(result);
+
+          //Assert
+          Assert.AreEqual(newDescription, result);
+        }
+
+        [TestMethod]
+        public void DeleteTask_DeleteATaskInDatabase_List()
+        {
+            //Arrange
+            Category testCategory = new Category("Household chores");
+            testCategory.Save();
+
+            DateTime newDateTime = new DateTime(2016,12,31);
+            DateTime newSecondDateTime = new DateTime(2015,1,1);
+            Task firstTask = new Task("Mow the lawn", testCategory.GetId(), newDateTime, newDateTime.ToString());
+            firstTask.Save();
+            Task secondTask = new Task("Do the dishes", testCategory.GetId(), newSecondDateTime, newSecondDateTime.ToString());
+            secondTask.Save();
+
+            List<Task> expectedList = new List<Task>{firstTask};
+
+            //Act
+            secondTask.DeleteTask(secondTask.GetId());
+            List<Task> result = testCategory.GetTasks();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedList, result);
+
+        }
+    }
+}
---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,224 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        private string _description;
+        private int _id;
+        private int _categoryId;
+        private DateTime _dueDate;
+        private string _stringDueDate;
+
+        public Task(string description, int categoryId, DateTime dueDate, string stringDueDate, int id = 0)
+        {
+            _description = description;
+            _categoryId = categoryId;
+            _dueDate = dueDate;
+            _stringDueDate = stringDueDate;
+            _id = id;
+        }
+
+        public override bool Equals(System.Object otherTask)
+        {
+          if (!(otherTask is Task))
+          {
+            return false;
+          }
+          else
+          {
+             Task newTask = (Task) otherTask;
+             bool idEquality = this.GetId() == newTask.GetId();
+             bool descriptionEquality = this.GetDescription() == newTask.GetDescription();
+             bool categoryEquality = this.GetCategoryId() == newTask.GetCategoryId();
+             return (idEquality && descriptionEquality && categoryEquality);
+           }
+        }
+        public override int GetHashCode()
+        {
+             return this.GetDescription().GetHashCode();
+        }
+
+        public string GetDueDate()
+        {
+          return _stringDueDate;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+        public int GetCategoryId()
+        {
+            return _categoryId;
+        }
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO tasks (description, category_id, due_date, due_date_string) VALUES (@description, @category_id, @due_date, @due_date_string);";
+
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@description";
+            description.Value = this._description;
+            cmd.Parameters.Add(description);
+
+            MySqlParameter categoryId = new MySqlParameter();
+            categoryId.ParameterName = "@category_id";
+            categoryId.Value = this._categoryId;
+            cmd.Parameters.Add(categoryId);
+
+            MySqlParameter dueDate = new MySqlParameter();
+            dueDate.ParameterName = "@due_date";
+            dueDate.Value = this._dueDate;
+            cmd.Parameters.Add(dueDate);
+
+            MySqlParameter dueDateString = new MySqlParameter();
+            dueDateString.ParameterName = "@due_date_string";
+            dueDateString.Value = this._stringDueDate;
+            cmd.Parameters.Add(dueDateString);
+
+            cmd.ExecuteNonQuery();
+            _id = (int) cmd.LastInsertedId;
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static List<Task> GetAll()
+        {
+            List<Task> allTasks = new List<Task> {};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM tasks;";
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+              int taskId = rdr.GetInt32(0);
+              string taskDescription = rdr.GetString(1);
+              int taskCategoryId = rdr.GetInt32(2);
+              string taskDueDate = rdr.GetString(4);
+              DateTime dummyDateTime = new DateTime();
+              Task newTask = new Task(taskDescription, taskCategoryId, dummyDateTime, taskDueDate, taskId);
+              allTasks.Add(newTask);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            Console.WriteLine(allTasks[0].GetDueDate());
+            return allTasks;
+        }
+        public static Task Find(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM tasks WHERE id = (@searchId);";
+
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = id;
+            cmd.Parameters.Add(searchId);
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            int taskId = 0;
+            string taskName = "";
+            int taskCategoryId = 0;
+            string taskDueDate = "";
+            DateTime dummyDateTime = new DateTime();
+
+            while(rdr.Read())
+            {
+              taskId = rdr.GetInt32(0);
+              taskName = rdr.GetString(1);
+              taskCategoryId = rdr.GetInt32(2);
+              taskDueDate = rdr.GetString(4);
+
+            }
+            Task newTask = new Task(taskName, taskCategoryId, dummyDateTime, taskDueDate, taskId);
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return newTask;
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM tasks;";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void UpdateDescription(string newDescription)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"UPDATE tasks SET description = @newDescription WHERE id = @searchId;";
+
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@newDescription";
+            description.Value = newDescription;
+            cmd.Parameters.Add(description);
+
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = _id;
+            cmd.Parameters.Add(searchId);
+
+            cmd.ExecuteNonQuery();
+            _description = newDescription;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+
+        }
+
+        public void DeleteTask(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM tasks WHERE id = @searchId;";
+
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = id;
+            cmd.Parameters.Add(searchId);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+    }
+}
